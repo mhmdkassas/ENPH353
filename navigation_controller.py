48d99f9c09da82500489b4b28f2b58e0228ab8d1
mhmdkassas <43212736+mhmdkassas@users.noreply.github.com>
2019-11-26 20:20:47 -0800

    Add files via upload

--- navigation_controller.py
+++ navigation_controller.py
@@ -32,7 +32,7 @@ class NavigationController():
         self.edge_val = [0]
         self.pos = 0
         self.white_val = []
-        self.ped = False
+        self.ped = True
         #self.last_edge = 0
 
     def img_callback(self, data):
@@ -51,29 +51,31 @@ class NavigationController():
         mask = cv2.inRange(hsv, lower, upper)
         # cv2.imshow("mask", mask)
         retVal, binary = cv2.threshold(self.cv_image, 64, 250, cv2.THRESH_BINARY)
-        cv2.imshow("real", self.cv_image)
-        # lower_bgr = np.uint8([[[41, 20, 14]]])
-        # upper_bgr = np.uint8([[[158, 134, 107]]])
+        # cv2.imshow("real", self.cv_image)
+        lower_bgr = np.uint8([[[41, 20, 14]]])
+        upper_bgr = np.uint8([[[158, 134, 107]]])
         l = np.asarray([104, 82, 41])
         u = np.asarray([113, 168, 158])
         hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, l, u)
         gb = cv2.GaussianBlur(mask, (5,5), cv2.BORDER_DEFAULT)
         r, b = cv2.threshold(gb, 64, 240, cv2.THRESH_BINARY)
-        center = int(float(b.shape[1])/2.0)
-        cropped_img = b[:, center: ]
+        h_center = int(float(b.shape[0])/2.0)
+        w_center = int(float(b.shape[1])/2.0)
+        cropped_img = b[h_center:, w_center: ]
         dims = cropped_img.shape
         h, w = dims[0], dims[1]
         crp = np.asarray(cropped_img)
         count = 0
-        for i in range(0, h):
-            for z in range(0, w):
-                if(crp.item(i, z) != 0):
-                    self.ped = True
-                    count = 1
-        if(count == 0):
-            self.ped = False
-        cv2.imshow("b", b)
+        # for i in range(0, h):
+        #     for z in range(0, w):
+        #         if(crp.item(i, z) != 0):
+        #             # self.ped = True
+        #             count = 1
+        # if(count == 0):
+        #     self.ped = False
+        # print(self.ped)
+        cv2.imshow("b", cropped_img)
         cv2.waitKey(3)
     def forward(self):
         # msg = Twist()
@@ -121,7 +123,9 @@ class NavigationController():
             if(pixel_arr.item(self.edge_height, i) != 0):
                 self.white_val.append(i)
         pos = max(self.white_val)  
-        # cv2.circle(self.cv_image, (pos, self.edge_height), 5, (0,0,255), -1)
+        cv2.circle(self.cv_image, (pos, self.edge_height), 5, (0,0,255), -1)
+        cv2.imshow("real",self.cv_image)
+        cv2.waitKey(3)
         return pos
 
     def edge_detector(self):
@@ -133,7 +137,7 @@ class NavigationController():
         for i in range(0, self.img_width - 1):
             if(pixel_array.item(self.edge_height, i) != 0):
                 self.edge_val.append(i)
-        print("num of edges {}" .format(len(self.edge_val)))
+        # print("num of edges {}" .format(len(self.edge_val)))
 
     def path_follower(self):
         self.speed = 1.0
@@ -145,18 +149,22 @@ class NavigationController():
         self.vel_msg.angular.z = 0
 
         if(center > position):
+            print("edge")
             self.vel_msg.linear.x = 0.0
             self.vel_msg.angular.z = 1
 
         if(center < position - tolerance):
+            print("left")
             self.vel_msg.linear.x = 0.5
             self.vel_msg.angular.z = -4
 
         if (center <= position and center >= position - tolerance):
+            print("f")
             self.vel_msg.linear.x = self.speed
             self.vel_msg.angular.z = 0
 
         if(1200 > position > dead_zone):
+            print("yikes")
             self.vel_msg.linear.x = 0
             self.vel_msg.angular.z = -0.3
 
@@ -193,9 +201,9 @@ class NavigationController():
         #     cv2.circle(self.cv_image, (i,
         #                                self.edge_height), 5,
         #                               (255, 0, 0), -1)
-        cv2.imshow("a", binary)
-        cv2.imshow("b", self.cv_image)
-        cv2.waitKey(3)
+        # cv2.imshow("a", binary)
+        # cv2.imshow("b", self.cv_image)
+        # cv2.waitKey(3)
 
 rospy.init_node('navigation', anonymous=True)
 rate = rospy.Rate(10)
@@ -221,9 +229,9 @@ while not rospy.is_shutdown():
         left_intersection = True
         robot.stop()
     else:
-        robot.img_test()
+        # robot.img_test()
         robot.edge_detector()
-        if(len(robot.edge_val) > 18 and robot.ped):
+        if(len(robot.edge_val) > 18):
             robot.stop()
 
         robot.path_follower()
