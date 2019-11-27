d4d4e59f49b161812e1efff0ae24e3fa950a206a
rhafeez221 <43095109+rhafeez221@users.noreply.github.com>
2019-11-26 21:55:07 -0800

    Add files via upload

--- navigation_controller.py
+++ navigation_controller.py
@@ -32,7 +32,8 @@ class NavigationController():
         self.edge_val = [0]
         self.pos = 0
         self.white_val = []
-        self.ped = True
+        self.ped = False
+        self.flag = True
         #self.last_edge = 0
 
     def img_callback(self, data):
@@ -52,21 +53,21 @@ class NavigationController():
         # cv2.imshow("mask", mask)
         retVal, binary = cv2.threshold(self.cv_image, 64, 250, cv2.THRESH_BINARY)
         # cv2.imshow("real", self.cv_image)
-        lower_bgr = np.uint8([[[41, 20, 14]]])
-        upper_bgr = np.uint8([[[158, 134, 107]]])
-        l = np.asarray([104, 82, 41])
-        u = np.asarray([113, 168, 158])
-        hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, l, u)
-        gb = cv2.GaussianBlur(mask, (5,5), cv2.BORDER_DEFAULT)
-        r, b = cv2.threshold(gb, 64, 240, cv2.THRESH_BINARY)
-        h_center = int(float(b.shape[0])/2.0)
-        w_center = int(float(b.shape[1])/2.0)
-        cropped_img = b[h_center:, w_center: ]
-        dims = cropped_img.shape
-        h, w = dims[0], dims[1]
-        crp = np.asarray(cropped_img)
-        count = 0
+        # lower_bgr = np.uint8([[[41, 20, 14]]])
+        # upper_bgr = np.uint8([[[158, 134, 107]]])
+        # l = np.asarray([104, 82, 41])
+        # u = np.asarray([113, 168, 158])
+        # hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        # mask = cv2.inRange(hsv, l, u)
+        # gb = cv2.GaussianBlur(mask, (5,5), cv2.BORDER_DEFAULT)
+        # r, b = cv2.threshold(gb, 64, 240, cv2.THRESH_BINARY)
+        # h_center = int(float(b.shape[0])/2.0)
+        # w_center = int(float(b.shape[1])/2.0)
+        # cropped_img = b[h_center:, w_center:]
+        # dims = cropped_img.shape
+        # h, w = dims[0], dims[1]
+        # crp = np.asarray(cropped_img)
+        # count = 0
         # for i in range(0, h):
         #     for z in range(0, w):
         #         if(crp.item(i, z) != 0):
@@ -75,8 +76,8 @@ class NavigationController():
         # if(count == 0):
         #     self.ped = False
         # print(self.ped)
-        cv2.imshow("b", cropped_img)
-        cv2.waitKey(3)
+        # cv2.imshow("b", cropped_img)
+        # cv2.waitKey(3)
     def forward(self):
         # msg = Twist()
         self.vel_msg.linear.x = self.speed
@@ -122,7 +123,7 @@ class NavigationController():
         for i in range(0, self.img_width -1):
             if(pixel_arr.item(self.edge_height, i) != 0):
                 self.white_val.append(i)
-        pos = max(self.white_val)  
+        pos = max(self.white_val)
         cv2.circle(self.cv_image, (pos, self.edge_height), 5, (0,0,255), -1)
         cv2.imshow("real",self.cv_image)
         cv2.waitKey(3)
@@ -138,8 +139,43 @@ class NavigationController():
             if(pixel_array.item(self.edge_height, i) != 0):
                 self.edge_val.append(i)
         # print("num of edges {}" .format(len(self.edge_val)))
+    def ped_finder(self):
+        center = int(float(self.img_width)/4.0)
+        hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        lower = np.asarray([110, 50, 50])
+        upper = np.asarray([130, 255, 255])
+        mask = cv2.inRange(hsv, lower, upper)
+        # cv2.imshow("mask", mask)
+        retVal, binary = cv2.threshold(self.cv_image, 64, 250, cv2.THRESH_BINARY)
+        cv2.imshow("real", self.cv_image)
+        # lower_bgr = np.uint8([[[41, 20, 14]]])
+        # upper_bgr = np.uint8([[[158, 134, 107]]])
+        l = np.asarray([104, 82, 41])
+        u = np.asarray([113, 168, 158])
+        hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, l, u)
+        gb = cv2.GaussianBlur(mask, (5,5), cv2.BORDER_DEFAULT)
+        r, b = cv2.threshold(gb, 64, 240, cv2.THRESH_BINARY)
+        cropped_img = b[:, center:]
+        cropped_img_1 = self.cv_image[:, center:]
+        dims = cropped_img.shape
+        h, w = dims[0], dims[1]
+        crp = np.asarray(cropped_img)
+        count = 0
+        for i in range(0, h):
+            for z in range(0, w):
+                if(crp.item(i, z) != 0):
+                    self.ped = True
+                    count = 1
+        if(count == 0):
+            self.ped = False
+        print(self.ped)
+        cv2.imshow("b", cropped_img)
+        cv2.imshow("r", cropped_img_1)
+        cv2.waitKey(3)
 
     def path_follower(self):
+        # self.ped_finder()
         self.speed = 1.0
         tolerance = 10
         dead_zone = 1000
@@ -149,48 +185,26 @@ class NavigationController():
         self.vel_msg.angular.z = 0
 
         if(center > position):
-            print("edge")
+            # print("edge")
             self.vel_msg.linear.x = 0.0
             self.vel_msg.angular.z = 1
 
         if(center < position - tolerance):
-            print("left")
+            # print("left")
             self.vel_msg.linear.x = 0.5
             self.vel_msg.angular.z = -4
 
         if (center <= position and center >= position - tolerance):
-            print("f")
+            # print("f")
             self.vel_msg.linear.x = self.speed
             self.vel_msg.angular.z = 0
 
         if(1200 > position > dead_zone):
-            print("yikes")
+            # print("yikes")
             self.vel_msg.linear.x = 0
             self.vel_msg.angular.z = -0.3
 
         self.vel_pub.publish(self.vel_msg)
-
-    # def ped_finder(self):
-    #     center = int(float(self.img_width)/2.0)
-    #     hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-    #     lower = np.asarray([110, 50, 50])
-    #     upper = np.asarray([130, 255, 255])
-    #     mask = cv2.inRange(hsv, lower, upper)
-    #     # cv2.imshow("mask", mask)
-    #     retVal, binary = cv2.threshold(self.cv_image, 64, 250, cv2.THRESH_BINARY)
-    #     cv2.imshow("real", self.cv_image)
-    #     # lower_bgr = np.uint8([[[41, 20, 14]]])
-    #     # upper_bgr = np.uint8([[[158, 134, 107]]])
-    #     l = np.asarray([104, 82, 41])
-    #     u = np.asarray([113, 168, 158])
-    #     hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-    #     mask = cv2.inRange(hsv, l, u)
-    #     gb = cv2.GaussianBlur(mask, (5,5), cv2.BORDER_DEFAULT)
-    #     r, b = cv2.threshold(gb, 64, 240, cv2.THRESH_BINARY)
-    #     cropped_img = b[:, center: ]
-    #     dims = cropped_img.shape
-    #     h, w = dims[0], dims[1]
-        
 
     def img_test(self):
         hsv_img = self.hsv_filter("white")
@@ -232,7 +246,16 @@ while not rospy.is_shutdown():
         # robot.img_test()
         robot.edge_detector()
         if(len(robot.edge_val) > 18):
-            robot.stop()
-
-        robot.path_follower()
-    
+            if(robot.flag):
+                robot.stop()
+                rospy.sleep((rospy.Duration(2)))
+                robot.ped_finder()
+                print(robot.ped)
+            if(robot.ped):
+                robot.stop()
+            if(robot.ped is False):
+                robot.path_follower()
+                robot.flag = False
+        else:
+            robot.flag = True
+            robot.path_follower()
